aa76920ff0488498d102036bbb6a392d0777c3e8
Kevin Quintana <kevinquintanaunjufi@gmail.com>
2024-01-31 19:27:16 -0300

    agregar modelo "ProfileImage.js" - server

--- server/src/models/ProfileImage.js
+++ server/src/models/ProfileImage.js
@@ -1,23 +1,24 @@
 const model = (sequelize, DataTypes) => {
-	const alias = 'Profession';
+	const alias = 'ProfileImage';
 	const cols = {
-		profession_id: {
+		image_id: {
 			type: DataTypes.INTEGER.UNSIGNED,
 			allowNull: false,
 			autoIncrement: true,
 			primaryKey: true,
 		},
-		profession_name: {
-			type: DataTypes.STRING(25),
+		image_name: {
+			type: DataTypes.STRING(36),
+			allowNull: true,
 		},
 	};
 	const config = {
-		tableName: 'professions',
+		tableName: 'profile_images',
 		timestamps: false,
 	};
 
-	const Profession = sequelize.define(alias, cols, config);
-	return Profession;
+	const ProfileImage = sequelize.define(alias, cols, config);
+	return ProfileImage;
 };
 
 module.exports = model;
