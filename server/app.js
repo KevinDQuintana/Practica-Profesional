efbf3f205a01ac32dfaa292a7f1cb2dde2c77ebf
Kevin Quintana <kevinquintanaunjufi@gmail.com>
2024-01-31 01:42:22 -0300

    agregar endpoint para servir imagenes - server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import applicants from './src/routes/applicants.js';
 import professions from './src/routes/professions.js';
+import imageSend from './src/routes/imageSend.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ app.get('/', (req, res) => res.send('Hello World!!!'));
 
 app.use('/applicants', applicants);
 app.use('/professions', professions);
+app.use('/image', imageSend);
 
 app.listen(port, () => {
 	console.log(`server started on http://localhost:${port}/`);
